95bd8cc8739fa9b933e3b110729e8d1e6a311325
Ashwin Paranjape <ashwing.2005@gmail.com>
2017-02-05 08:24:23 -0800

    Turk API and associated files

--- turkApi/create_batch.py
+++ turkApi/create_batch.py
@@ -7,6 +7,9 @@ import ConfigParser
 import sys, os
 import time
 import pandas as pd
+import variablePricing
+import codecs
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('doc_paths_file', nargs=1, type=argparse.FileType('r'), default=sys.stdin, help="File or stdin containing documents paths")
@@ -18,10 +21,25 @@ mtc = connect(config.get('default', 'target'))
 params = {'target': config.get('default', 'target')}
 base_url = config.get('default', 'interface')+'?'
 batch_info = []
-for path in args.doc_paths_file[0]:
-    params['doc_id'] = 'exhaustive/'+path.strip()
+total_cost = 0
+for doc_id in args.doc_paths_file[0]:
+    doc_id = doc_id.strip()
+    params['doc_id'] = config.get('default', 'doc_path_server')+doc_id.strip()
+    doc = json.load(codecs.open(os.path.join(config.get('default', 'doc_path_script'), doc_id), 'r','utf-8'))
+    if config.has_option('default', 'units_function') and config.has_option('default', 'unit_reward'):
+        length = getattr(variablePricing, config.get('default', 'units_function'))(doc)
+        reward = config.getfloat('default', 'unit_reward') * length
+        est_time = int(config.getfloat('default', 'unit_time') * length)
+        reward = float("{0:.2f}".format(reward))
+        params['est_time'] = est_time
+        params['reward'] = "{0:.2f}".format(reward)
+        print length,  reward, params
+    else:
+        reward = config.getfloat('default', 'doc_reward')
+        print reward, params
     url = base_url + urllib.urlencode(params)
     question = ExternalQuestion(url, config.get('default', 'frame_height'))
+    total_cost += reward
     response = mtc.create_hit(
             question=question,
             title=config.get('default', 'title'),
@@ -30,7 +48,7 @@ for path in args.doc_paths_file[0]:
             duration = config.get('default', 'duration'), 
             annotation = params['doc_id'], 
             lifetime = config.get('default', 'lifetime'), 
-            reward = config.get('default', 'reward'))
+            reward = reward)
     batch_info.append({'docId': params['doc_id'], 'HITTypeId': response[0].HITTypeId, 'HITId': response[0].HITId, })
 metadata_dir = './batch/'
 if not os.path.exists(metadata_dir):
@@ -38,6 +56,7 @@ if not os.path.exists(metadata_dir):
 timestr = time.strftime("%Y%m%d-%H%M%S")
 metadata_filepath = metadata_dir+args.config_file.split('/')[-1]+'.'+timestr+'.csv'
 pd.DataFrame(batch_info).to_csv(path_or_buf = metadata_filepath, sep = '\t', index=False)
+print "Total cost for all assignments", total_cost * config.getint('default', 'max_assignments')
 
  
 
