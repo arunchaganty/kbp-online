03a91221f31ffe160d2dd1dd799e2708417cc4e5
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-02-06 04:35:24 -0800

    Updated submission script.

--- scripts/get_submissions.sh
+++ scripts/get_submissions.sh
@@ -1,7 +1,10 @@
 KBS=(kb_alternate_names_2016_8_28_16 kb_patterns_2016_8_28_16 kb_rnn_unk_2016_8_28_16 kb_supervised_2016_8_28_16 kb_website_2016_8_28_16)
 #KBS_=(kb_patterns_all_2016_8_28_16 kb_rnn_unk_2016_8_28_16 kb_supervised_2016_8_28_16)
-KBS_=(kb_patterns_2016_8_28_16 kb_rnn_unk_2016_8_28_16 kb_supervised_2016_8_28_16)
-SAMPLES=250
+#KBS_=(kb_patterns_2016_8_28_16 kb_rnn_unk_2016_8_28_16 kb_supervised_2016_8_28_16)
+KBS_=(kb_rnn_unk_2016_8_28_16 kb_supervised_2016_8_28_16)
+#KBS_=(kb_patterns_2016_8_28_16)
+SAMPLES=500
+SEED=51
 
 #for kb in ${KBS[@]}; do
 #  echo $kb
@@ -13,18 +16,18 @@ SAMPLES=250
 OLD_ENTRIES=
 for kb in ${KBS_[@]}; do
   echo $kb;
-  python3 scripts/prepare_pooled_data.py sample -i data/$kb.tsv -o data/pooled-$kb.entities.$SAMPLES.tsv -n $SAMPLES $OLD_ENTRIES;
-  OLD_ENTRIES="$OLD_ENTRIES data/pooled-$kb.entities.$SAMPLES.tsv"
-  python3 scripts/prepare_pooled_data.py sample -r -i data/$kb.tsv -o data/pooled-$kb.relations.$SAMPLES.tsv -n $SAMPLES $OLD_ENTRIES;
-  OLD_ENTRIES="$OLD_ENTRIES data/pooled-$kb.relations.$SAMPLES.tsv"
+  python3 scripts/prepare_pooled_data.py sample -s $SEED -i data/$kb.tsv -o data/pooled-$kb.entities.$SEED.$SAMPLES.tsv -n $SAMPLES $OLD_ENTRIES;
+  OLD_ENTRIES="$OLD_ENTRIES data/pooled-$kb.entities.$SEED.$SAMPLES.tsv"
+  python3 scripts/prepare_pooled_data.py sample -s $SEED -r -i data/$kb.tsv -o data/pooled-$kb.relations.$SEED.$SAMPLES.tsv -n $SAMPLES $OLD_ENTRIES;
+  OLD_ENTRIES="$OLD_ENTRIES data/pooled-$kb.relations.$SEED.$SAMPLES.tsv"
 done;
 
 for kb in ${KBS_[@]}; do
   echo $kb;
-  python3 scripts/prepare_pooled_data.py make -i data/pooled-$kb.entities.$SAMPLES.tsv -o data/pooled-$kb.entities.$SAMPLES/;
-  python3 scripts/prepare_pooled_data.py make -i data/pooled-$kb.relations.$SAMPLES.tsv -o data/pooled-$kb.relations.$SAMPLES/;
+  python3 scripts/prepare_pooled_data.py make -i data/pooled-$kb.entities.$SEED.$SAMPLES.tsv -o data/pooled-$kb.entities.$SEED.$SAMPLES/;
+  python3 scripts/prepare_pooled_data.py make -i data/pooled-$kb.relations.$SEED.$SAMPLES.tsv -o data/pooled-$kb.relations.$SEED.$SAMPLES/;
   cd data;
-  tar -czf pooled-$kb.entities.$SAMPLES.tar.gz pooled-$kb.entities.$SAMPLES/;
-  tar -czf pooled-$kb.relations.$SAMPLES.tar.gz pooled-$kb.relations.$SAMPLES/;
+  tar -czf pooled-$kb.entities.$SEED.$SAMPLES.tar.gz pooled-$kb.entities.$SEED.$SAMPLES/;
+  tar -czf pooled-$kb.relations.$SEED.$SAMPLES.tar.gz pooled-$kb.relations.$SEED.$SAMPLES/;
   cd -
 done;
