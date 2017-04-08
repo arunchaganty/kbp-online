11a7a98128784c4b210cead7eb8d96d65ca94c01
Ashwin Paranjape <ashwing.2005@gmail.com>
2017-04-07 23:29:00 -0700

    Delete moved files

--- turkApi/approve_assignments.py
+++ turkApi/approve_assignments.py
@@ -1,5 +1,5 @@
 import boto
-from boto.mturk.connection import MTurkConnection
+from boto.mturk.connection import MTurkConnection, MTurkRequestError
 from boto.mturk.question  import ExternalQuestion
 from connection import connect
 import urllib
@@ -17,7 +17,22 @@ config = ConfigParser.ConfigParser()
 config.read(args.config_file)
 mtc = connect(config.get('default', 'target'))
 answers_file = pd.read_csv(args.answers_file[0], sep='\t')
+batchname = args.answers_file[0].name.split('/')[-1]
+approved = []
 for assignmentId in answers_file['assignmentId']:
-    mtc.approve_assignment(assignmentId)
+    try: 
+        x = mtc.approve_assignment(assignmentId)
+        print "Approved", assignmentId
+        approved.append({'assignmentId': assignmentId, 'response': x})
+    except MTurkRequestError as err:
+        print err
+
+approved_dir = './approved/'
+if not os.path.exists(approved_dir):
+    os.makedirs(approved_dir) 
+approved_filepath = approved_dir+batchname
+pd.DataFrame(approved).to_csv(path_or_buf = approved_filepath, sep = '\t', index=False, 
+        columns=['assignmentId', 'response'],
+        encoding='utf-8')
 
 
--- turkApi/retrieve_answers.py
+++ turkApi/retrieve_answers.py
@@ -18,33 +18,40 @@ config.read(args.config_file)
 mtc = connect(config.get('default', 'target'))
 answer_field = config.get('default', 'answer_field')
 metadata = pd.read_csv(args.metadata_file[0], sep='\t')
+batchname = args.metadata_file[0].name.split('/')[-1]
 answers = []
-for HITId in metadata['HITId']:
+for idx, row in metadata.iterrows():
+    HITId = row['HITId']
     assignments = mtc.get_assignments(HITId)
+    units = -1 if not 'length' in row else row['length']
+    reward = -1 if not 'reward' in row else row['reward']
     for assignment in assignments:
         print dir(assignment)
         worker_id = assignment.WorkerId
-        worker_answer = '10'
+        worker_answer = ''
         doc_id = ''
         comment = ''
+        timeTaken = ''
         for answer in assignment.answers[0]:
-            print answer.qid
+            #print answer.qid
             if answer.qid == answer_field:
                 worker_answer = answer.fields[0]
             if answer.qid == 'docId':
                 doc_id = answer.fields[0]
             if answer.qid == 'comment':
                 comment = answer.fields[0]
+            if answer.qid == 'td':
+                timeTaken = answer.fields[0]
 #                print u"The Worker with ID {} for HITId {} gave the answer {}".format(worker_id, HITId, worker_answer)
-        answers.append({'workerId': worker_id, 'assignmentId': assignment.AssignmentId, 'HITId': HITId, answer_field: worker_answer, 'doc_id': doc_id, 'comments': comment})
+        print assignment.AssignmentId, doc_id, timeTaken, comment
+        answers.append({'workerId': worker_id, 'assignmentId': assignment.AssignmentId, 'HITId': HITId, answer_field: worker_answer, 'doc_id': doc_id, 'comments': comment, 'timeTaken': timeTaken, 'units': units, 'reward': reward})
 
-print answers
+#print answers
 answers_dir = './answers/'
 if not os.path.exists(answers_dir):
     os.makedirs(answers_dir) 
-timestr = time.strftime("%Y%m%d-%H%M%S")
-answers_filepath = answers_dir+args.config_file.split('/')[-1]+'.'+timestr+'.csv'
+answers_filepath = answers_dir+batchname
 pd.DataFrame(answers).to_csv(path_or_buf = answers_filepath, sep = '\t', index=False, 
-        columns=['doc_id', 'HITId', 'workerId','assignmentId', 'comments', answer_field],
+        columns=['doc_id', 'HITId', 'workerId','assignmentId', 'timeTaken', 'units', 'reward', 'comments', answer_field, ],
         encoding='utf-8')
 
