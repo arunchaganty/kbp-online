da4228d8eb61d3eafa795d332dc64f065135d558
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-04-26 20:00:16 -0700

    Starting explore/ view

--- src/web/urls.py
+++ src/web/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^submit/$', views.submit, name='submit'),
-    url(r'^explore/$', views.explore, name='explore'), # TODO: expand to include different documents, etc.
+    url(r'^explore/(?P<doc_id>[a-zA-Z_0-9.]+)/$', views.explore, name='explore'),
+    url(r'^explore/$', views.explore, name='explore'),
     url(r'^$', views.home, name='home'),
 ]
