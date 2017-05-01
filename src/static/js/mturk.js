9daff5dd127903af9d1bdb0ab733a2c25be6c936
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-04-30 22:01:02 -0700

    Using RequireJS with javascript

--- src/static/js/mturk.js
+++ src/static/js/mturk.js
@@ -1,56 +1,63 @@
-function TurkHelper(){
-    st = new Date().getTime()/1000;
-    this.getUrlVars = function()
-    {
-        var vars = {}, hash;
-        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-        for(var i = 0; i < hashes.length; i++)
-        {
-	    hashes[i] = decodeURIComponent(hashes[i])
-            hash = hashes[i].split('=');
-            vars[hash[0]] = hash[1];
-        }
-        return vars;
-    };
-    this.urlVars = this.getUrlVars();
-    this.activated = false;
-    if ('assignmentId' in this.urlVars){
-        this.activated = true;
-    }
-    else{
-        return;
-    }
-    this.preview = true;
-    if(this.urlVars['assignmentId'] != 'ASSIGNMENT_ID_NOT_AVAILABLE'){
-        this.preview = false;
-    }
-    if(this.activated){
-        var submitUrl = "";
-        if(this.preview){
-  	    $("#done")[0].disabled = true;
-            return;
-        }
-        if ('target' in this.urlVars){
-            if(this.urlVars['target'] == 'sandbox'){
-                submitUrl = "https://workersandbox.mturk.com/mturk/externalSubmit";
-            }
-            else if(this.urlVars['target'] == 'actual'){
-                submitUrl = "https://www.mturk.com/mturk/externalSubmit";
-            }
-        }
+/*!
+ * KBPOnline: mechanical turk helper script.
+ * Author: Ashwin Paranjape
+ * Licensed under the MIT license
+ */
 
-        $('#mturk_form').attr('action', submitUrl);
-        $("#assignmentId").attr('value', this.urlVars['assignmentId']);
-        $("#docId").attr('value', this.urlVars['doc_id']);
-	console.log('all ready to submit');
+// Gets the URL variables stored in the GET string.
+function getURLVars() {
+  var vars = {}, hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for(var i = 0; i < hashes.length; i++) {
+    hashes[i] = decodeURIComponent(hashes[i]);
+    hash = hashes[i].split('=');
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
+}
+
+// Sets up submit form to interact with mturk
+function TurkHelper() {
+  // Records start time
+  st = new Date().getTime()/1000;
+
+  this.urlVars = getUrlVars();
+  this.activated = false;
+  if ('assignmentId' in this.urlVars) {
+    this.activated = true;
+  }
+  else {
+    return;
+  }
+  this.preview = true;
+  if (this.urlVars.assignmentId != 'ASSIGNMENT_ID_NOT_AVAILABLE') {
+    this.preview = false;
+  }
+  if (this.activated) {
+    var submitUrl = "";
+    if (this.preview) {
+      $("#done")[0].disabled = true;
+      return;
     }
+    if ('target' in this.urlVars) {
+      if (this.urlVars.target == 'sandbox') {
+        submitUrl = "https://workersandbox.mturk.com/mturk/externalSubmit";
+      }
+      else if (this.urlVars.target == 'actual') {
+        submitUrl = "https://www.mturk.com/mturk/externalSubmit";
+      }
+    }
+
+    $('#mturk_form').attr('action', submitUrl);
+    $("#assignmentId").attr('value', this.urlVars.assignmentId);
+    $("#docId").attr('value', this.urlVars.doc_id);
+    console.log('all ready to submit');
+  }
 }
-//turkHelper.prototype.previewHandlers = [];
-TurkHelper.prototype.initialize_doc = function(doc_id){
-    $.getJSON(doc_id, initialize_interface)
+
+TurkHelper.prototype.initialize_doc = function(doc_id) {
+  $.getJSON(doc_id, initialize_interface)
     .fail(function() {
-        console.log("Could not load data.");
+      console.log("Could not load data.");
     });
-}
-/*mainInterface.doneListeners.push(function(){
-});*/
+};
---
+++ src/static/js/widgets.js
@@ -0,0 +1,9 @@
+/*!
+ * KBPOnline: widgets listing
+ * Author: Arun Chaganty, Ashwin Paranjape
+ * Licensed under the MIT license
+ */
+
+// DocWidget
+
+
