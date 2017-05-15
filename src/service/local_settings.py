d6172bdbd3962fb18f2f7cd25fecf3f51acd7209
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-05-15 13:43:34 -0700

    Added local settings

---
+++ src/service/local_settings.py
@@ -0,0 +1,46 @@
+from .base_settings import *
+
+# Debug
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'test': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kbpo_test',
+        'USER': 'kbpo',
+        'PASSWORD': 'kbpo',
+        'HOST': 'localhost',
+        'PORT': 5433, # Tunnel to kbpo.stanford.edu
+    },
+    'production': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kbpo',
+        'USER': 'kbpo',
+        'PASSWORD': 'kbpo',
+        'HOST': 'localhost',
+        'PORT': 5433, # Tunnel to kbpo.stanford.edu
+    },
+}
+DATABASES['default'] = DATABASES['test']
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
