097b8ef3c7f7d785c6f334a60345b3b9201a6343
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-05-18 21:28:43 -0700

    Removed obsolete entity sampling functions from db_evaluation.py

--- src/kbpo/db_evaluation.py
+++ src/kbpo/db_evaluation.py
@@ -11,102 +11,6 @@ from . import db
 
 logger = logging.getLogger(__name__)
 
-def compute_exhaustive_distribution(corpus_tag):
-    distribution = Counter()
-    for row in db.select("""
-        WITH _counts AS (
-            SELECT COUNT(*) FROM evaluation_relation e, evaluation_batch b
-            WHERE e.question_batch_id = b.id AND b.batch_type = 'exhaustive_relations' AND b.corpus_tag = %(tag)s)
-        SELECT subject_id, relation, object_id, 1./c.count AS prob 
-        FROM evaluation_relation e, evaluation_batch b, _counts c
-        WHERE e.question_batch_id = b.id AND b.batch_type = 'exhaustive_relations'
-          AND b.corpus_tag = %(tag)s
-        """, tag=corpus_tag):
-        distribution[row.subject_id, row.relation, row.object_id] = float(row.prob)
-    return distribution
-
-def test_compute_exhaustive_distribution():
-    tag = 'kbp2016'
-    distribution = compute_exhaustive_distribution(tag)
-    value = sum(distribution.values())
-    assert abs(value - 1.0) < 1.e-5, "Distribution is not normalized: Z = {}".format(value)
-
-# Get distributions.
-def compute_instance_distribution(corpus_tag, submission_id):
-    distribution = Counter()
-    for row in db.select("""
-        WITH _counts AS (SELECT submission_id, COUNT(*) FROM submission_relation s GROUP BY submission_id)
-        SELECT s.subject_id, s.relation, s.object_id, 1./c.count AS prob
-        FROM submission_relation s, _counts c, submission s_
-        WHERE s.submission_id = c.submission_id AND s.submission_id = s_.id
-          AND s.submission_id = %(submission_id)s
-          AND s_.corpus_tag = %(tag)s
-        """, tag=corpus_tag, submission_id=submission_id):
-        distribution[row.subject_id, row.relation, row.object_id] = float(row.prob)
-    return distribution
-
-def test_compute_instance_distribution():
-    tag = 'kbp2016'
-    for submission in get_submissions():
-        P = compute_instance_distribution(tag, submission.id)
-        Z = sum(P.values())
-        assert abs(Z - 1.0) < 1.e-5, "Distribution for {} is not normalized: Z = {}".format(submission.id, Z)
-
-def compute_relation_distribution(corpus_tag, submission_id):
-    distribution = Counter()
-    for row in db.select("""
-        WITH _relations AS (SELECT submission_id, COUNT(DISTINCT relation) FROM submission_relation s GROUP BY submission_id),
-             _counts AS (SELECT submission_id, relation, COUNT(*) FROM submission_relation s GROUP BY submission_id, relation)
-        SELECT s.subject_id, s.relation, s.object_id, (1./c.count)/(r.count) AS prob
-        FROM submission_relation s, _counts c, _relations r, submission s_
-        WHERE s.submission_id = r.submission_id AND s.submission_id = c.submission_id AND s.relation = c.relation
-          AND s.submission_id = %(submission_id)s
-          AND s_.corpus_tag = %(tag)s
-        """, tag=corpus_tag, submission_id=submission_id):
-        distribution[row.subject_id, row.relation, row.object_id] = float(row.prob)
-    return distribution
-
-def test_compute_relation_distribution():
-    tag = 'kbp2016'
-    for submission in get_submissions():
-        P = compute_relation_distribution(tag, submission.id)
-        Z = sum(P.values())
-        assert abs(Z - 1.0) < 1.e-5, "Distribution for {} is not normalized: Z = {}".format(submission.id, Z)
-
-def compute_entity_distribution(corpus_tag, submission_id):
-    distribution = Counter()
-    for row in db.select("""
-        WITH _counts AS (
-                SELECT s.submission_id, link_name, COUNT(*) 
-                FROM submission_relation s, submission_link l, submission_mention m
-                WHERE s.submission_id = l.submission_id AND s.submission_id = m.submission_id
-                AND s.doc_id = l.doc_id AND s.doc_id = m.doc_id
-                AND  s.subject_id = m.mention_id AND m.canonical_id = l.mention_id
-                GROUP BY l.link_name, s.submission_id),
-        _entities AS (
-                SELECT submission_id, COUNT(*)
-                FROM _counts
-                GROUP BY submission_id)
-        SELECT s.subject_id, s.relation, s.object_id, l.link_name, (1./c.count)/(e.count) AS prob
-        FROM submission_relation s, submission_mention m, submission_link l, _counts c, _entities e, submission s_
-        WHERE s.submission_id = l.submission_id AND s.submission_id = m.submission_id
-          AND s.doc_id = l.doc_id AND s.doc_id = m.doc_id
-          AND s.subject_id = m.mention_id AND m.canonical_id = l.mention_id
-          AND s.submission_id = e.submission_id AND s.submission_id = c.submission_id AND c.link_name = l.link_name
-          AND s_.id = s.submission_id
-          AND s.submission_id = %(submission_id)s
-          AND s_.corpus_tag = %(tag)s
-        """, tag=corpus_tag, submission_id=submission_id):
-        distribution[row.subject_id, row.relation, row.object_id] = float(row.prob)
-    return distribution
-
-def test_compute_entity_distribution():
-    tag = 'kbp2016'
-    for submission in get_submissions():
-        P = compute_entity_distribution(tag, submission.id)
-        Z = sum(P.values())
-        assert abs(Z - 1.0) < 1.e-5, "Distribution for {} is not normalized: Z = {}".format(submission.id, Z)
-
 def get_exhaustive_samples(corpus_tag):
     rows = db.select("""
         SELECT e.doc_id, e.subject_id, e.object_id, e.relation, e.weight
@@ -135,9 +39,9 @@ def get_submission_samples(corpus_tag, scheme, submission_id):
           AND b.params ~ %(submission_f)s
           AND r.submission_id = %(submission_id)s 
           """, tag=corpus_tag,
-                    scheme='"method":"{}"'.format(scheme),
-                    submission_id=submission_id,
-                    submission_f='"submission_id":{}'.format(submission_id)
+                     scheme='"method":"{}"'.format(scheme),
+                     submission_id=submission_id,
+                     submission_f='"submission_id":{}'.format(submission_id)
                     )
     # TODO: ^^ is a hack to get the right rows from the database. we
     # should probably do differently.
