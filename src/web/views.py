4d63bb51621196f0b4636491a871d46aefef7b50
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2017-05-21 19:47:31 -0700

    Added list corpus

--- src/web/views.py
+++ src/web/views.py
@@ -43,11 +43,10 @@ def explore_corpus(request, corpus_tag, doc_id=None):
     kbpo.interface functions.
     """
     if doc_id is None:
-        # List corpus_tags.
-        doc = Document.objects.order_by('?').first()
-        return redirect('explore', doc_id=doc.id)
+        # List all documents for a corpus_tag (?).
+        return render(request, 'explore_corpus.html', {'corpus_tag': corpus_tag})
     doc = get_object_or_404(Document, id=doc_id)
-    return render(request, 'explore.html', {'doc_id': doc.id})
+    return render(request, 'explore_document.html', {'doc_id': doc.id})
 
 def explore_submission(request, submission_id):
     """
@@ -232,14 +231,16 @@ def api_evaluation_mention_pairs(_, doc_id, subject_id=None, object_id=None):
 
     return JsonResponse(ret, safe=False)
 
-def api_submission_entries(request, submission_id):
+def api_submission_entries(_, submission_id):
     """
     Get all the submitted relations from submission_id.
     """
-    if request.GET:
-        evaluated_only = bool(request.GET["evaluated_only"])
-    else:
-        evaluated_only = True
-
     ret = api.get_submission_entries(submission_id)
+    return JsonResponse(ret, safe=False)
+
+def api_corpus_listing(_, corpus_tag):
+    """
+    Get all the submitted relations from submission_id.
+    """
+    ret = api.get_corpus_listing(corpus_tag)
     return JsonResponse(ret, safe=False)
