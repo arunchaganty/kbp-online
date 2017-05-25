55d26f19f0423ce783f97f99baaaca057c40ceeb
Ashwin Paranjape <ashwing.2005@gmail.com>
2017-05-24 23:42:36 -0700

    Added function to convert to numeric range with bound check

--- src/kbpo/schema.py
+++ src/kbpo/schema.py
@@ -4,12 +4,20 @@
 Database schema as namedtuples
 """
 from collections import namedtuple
+from psycopg2.extras import NumericRange
 
 _Provenance = namedtuple("Provenance", ["doc_id", "begin", "end"])
 class Provenance(_Provenance):
     def __new__(cls, doc_id, begin, end):
         assert begin <= end, "Invalid span, expected begin {} <= end {} for provenance {}:{}-{}".format(begin, end, doc_id, begin, end)
         return super(Provenance, cls).__new__(cls, doc_id, begin, end)
+
+def getNumericRange(begin, end):
+    if begin >= end:
+        raise IndexError(begin, end)
+    return NumericRange(begin, end)
+
+
 MentionInstance = namedtuple("MentionInstance", ["doc_id", "span", "canonical_span", "mention_type", "gloss", "weight"])
 LinkInstance = namedtuple("LinkInstance", ["doc_id", "span", "link_name", "weight"])
 RelationInstance = namedtuple("RelationInstance", ["doc_id", "subject", "object", "relation", "weight"])
