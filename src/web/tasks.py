c671be0aacd0de9d53d4d115b146f3908cd44df3
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2017-06-27 15:33:45 -0700

    New and improved create_evaluation_batch_for_submission

--- src/web/tasks.py
+++ src/web/tasks.py
@@ -16,7 +16,7 @@ from kbpo import api
 from kbpo.parser import MFileReader, TacKbReader
 from kbpo.evaluation_api import get_updated_scores, update_score
 from kbpo.sampling import sample_submission as _sample_submission
-from kbpo.questions import create_evaluation_batch_from_submission_sample
+from kbpo.questions import create_evaluation_batch_for_submission
 from kbpo.turk import connect, create_batch
 
 from .models import Submission, SubmissionState
@@ -129,7 +129,7 @@ def sample_submission(submission_id, type_='entity_relation', n_samples = 500):
 
     logger.info("Sampling submission %s", submission_id)
     if state.status != 'pending-sampling':
-        logger.warning("Trying to process submission %s, but state is %s", submission, state.status)
+        logger.warning("Trying to sample submission %s, but state is %s", submission, state.status)
         return
 
     try:
@@ -147,43 +147,44 @@ def sample_submission(submission_id, type_='entity_relation', n_samples = 500):
         state.save()
 
 @shared_task
-def turk_submission(sample_batch_id):
+def turk_submission(submission_id):
     """
     Takes care of turking from a submission from _sample.
     """
-    submission_id = list(db.select("""
-        SELECT submission_id FROM sample_batch WHERE id = %(batch_id)s
-        """, batch_id = sample_batch_id))
-    assert len(submission_id) == 1, "Sample batch {} does not exist!".format(sample_batch_id)
-
-    submission_id = submission_id[0].submission_id
     assert Submission.objects.filter(id=submission_id).count() > 0, "Submission {} does not exist!".format(submission_id)
     assert SubmissionState.objects.filter(submission_id=submission_id).count() > 0, "SubmissionState {} does not exist!".format(submission_id)
     submission = Submission.objects.get(id=submission_id)
     state = SubmissionState.objects.get(submission_id=submission_id)
 
-    logger.info("Turking sample %s (submission %s)", sample_batch_id, submission_id)
+    logger.info("Turking submission %s", submission.id)
 
     if state.status != 'pending-turking':
-        logger.warning("Trying to process submission %s, but state is %s", submission.id, state.status)
+        logger.warning("Trying to turk submission %s, but state is %s", submission.id, state.status)
         return
     try:
-        evaluation_batch_id = create_evaluation_batch_from_submission_sample(sample_batch_id)
-        assert evaluation_batch_id is not None, "Evaluation batch not created"
-        evaluation_batch = api.get_question_batch(evaluation_batch_id)
-        questions = api.get_questions(evaluation_batch_id)
+        sample_batches = api.get_submission_sample_batches(submission_id)
+        assert len(sample_batches) > 0, "No sample batches to turk for submission {}".format(submission_id)
 
-        mturk_connection = connect(MTURK_TARGET)
-        create_batch(mturk_connection, evaluation_batch.id, evaluation_batch.batch_type, questions)
 
+        evaluation_batch_id = create_evaluation_batch_for_submission(submission_id)
+        if evaluation_batch_id is None:
+            logger.warning("Evaluation batch not created because all possible questions have been asked!")
+        else:
+            evaluation_batch = api.get_question_batch(evaluation_batch_id)
+            questions = api.get_questions(evaluation_batch_id)
+
+            mturk_connection = connect(MTURK_TARGET)
+            create_batch(mturk_connection, evaluation_batch.id, evaluation_batch.batch_type, questions)
+
+        # Move state forward.
         state.status = 'pending-annotation'
         state.save()
-
     except Exception as e:
         logger.exception(e)
         state.status = 'error'
         state.message = str(e)
         state.save()
+
 @shared_task
 def process_response(assignment_id):
     """
